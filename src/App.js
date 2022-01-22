5e774533f6dc036f21f7a6c09442f0bef0fa9c91
idmzulkarnaen <kreatifku1995@gmail.com>
2022-01-22 17:10:03 +0700

    component calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Calculator from "./components/Calculator";
 
 function App() {
-  return (
-    <div className="App">Test</div>
-  );
+  return <Calculator />;
 }
 
 export default App;
