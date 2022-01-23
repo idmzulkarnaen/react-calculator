2bf6190f335941ec8baeecacdafbb30a6706f679
idmzulkarnaen <kreatifku1995@gmail.com>
2022-01-23 16:09:01 +0700

    fungsi delete n all clear

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -3,7 +3,9 @@ import {Container, Screen, Previous, Current, Button} from './Styled'
 
 export default function Calculator() {
 
+  const [previous, setPrevious] = useState('')
   const [current, setCurrent] = useState('')
+  const [operation, setOperation] = useState('')
 
   const appendValue = (el) => {
     // test 1 tombol ketika dklik. Sebelum atribut yang dibawah didalam {} ada
@@ -15,14 +17,24 @@ export default function Calculator() {
     setCurrent(current+value)
   }
 
+  const handleDelete = () => {
+    setCurrent(String(current).slice(0, -1))
+  }
+
+  const handleAllClear = () => {
+    setCurrent('')
+    setPrevious('')
+    setOperation('')
+  }
+
   return (
     <Container>
         <Screen>
-            <Previous>10 +</Previous>
+            <Previous>{previous} {operation}</Previous>
             <Current>{current}</Current>
         </Screen>
-        <Button gridSpan={2} control>AC</Button>
-        <Button control>DEL</Button>
+        <Button onClick={handleAllClear} gridSpan={2} control>AC</Button>
+        <Button onClick={handleDelete} control>DEL</Button>
         <Button operation>÷</Button>
         <Button data={'7'} onClick={appendValue}>7</Button>
         <Button data={'8'} onClick={appendValue}>8</Button>
